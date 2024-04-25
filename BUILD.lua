05ad9fda217b7f882d9f1533ecd00296ab94f018
samcarter <sam.carter8@aol.com>
2024-04-25 02:02:39 +0200

    change capitalisation II

---
+++ BUILD.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env texlua
+
+-- Execute with ======================================================
+-- l3build tag
+-- l3build ctan
+-- <check announcement.txt and format if necessary>
+-- l3build upload
+-- l3build clean
+
+-- Settings ==========================================================
+module = "beamertheme-tcolorbox"
+ctanpkg = "beamertheme-tcolorbox"
+ctanprefix = "/macros/latex/contrib/beamer-contrib/themes/"
+ctansummary = "A beamer inner theme which reproduces standard beamer blocks using tcolorboxes"
+
+-- common settings =============================================================
+local common_settings, build_settings = pcall(require, "../beamertheme-sam/build-settings.lua")
